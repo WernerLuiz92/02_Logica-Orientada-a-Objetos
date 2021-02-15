8ed622d574c6e0fb5439127e08c66281bbecbcb3
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2021-02-15 16:42:38 -0300

    Aula 5-8 -- Utilizando nossas Classes

---
+++ Java/Aula 4-5 -- zoologico/src/br/com/treinaweb/zoologico/app/Programa.java
@@ -0,0 +1,27 @@
+package br.com.treinaweb.zoologico.app;
+
+import br.com.treinaweb.zoologico.classes.*;
+
+public class Programa {
+
+	public static void main(String[] args) {
+
+		Animal cachorro = new Animal();
+		
+		cachorro.setName("Princesa");
+		cachorro.setSpecies("Canina");
+		cachorro.setAge(4);
+		
+		System.out.println("Olá, seu animal é da espécie " + cachorro.getSpecies());
+		System.out.println("O nome do seu animal é " + cachorro.getName());
+		System.out.println("A idade do seu animal é de " + cachorro.getAge() + " anos");
+		if (cachorro.isAdult()) {
+			System.out.println("Seu animal é considerado adulto");
+		} else {
+			System.out.println("Seu animal é considerado filhote");
+		}
+		
+		
+	}
+
+}
