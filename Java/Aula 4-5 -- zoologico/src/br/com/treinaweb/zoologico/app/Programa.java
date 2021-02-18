b0e4cd1eab3c6813be79f54d74d6771f6f9ee270
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2021-02-18 17:54:53 -0300

    .

--- Java/Aula 4-5 -- zoologico/src/br/com/treinaweb/zoologico/app/Programa.java
+++ Java/Aula 4-5 -- zoologico/src/br/com/treinaweb/zoologico/app/Programa.java
@@ -7,11 +7,23 @@ public class Programa {
 	public static void main(String[] args) {
 
 		Cachorro cachorro = new Cachorro("Princesa", 4, "Canina");
+		Gato gato = new Gato("Missie", 6, "Felina");
 		
 		System.out.println("Olá, seu animal é da espécie " + cachorro.getSpecies());
 		System.out.println("O nome do seu animal é " + cachorro.getName());
 		System.out.println("A idade do seu animal é de " + cachorro.getAge() + " anos");
 		if (cachorro.isAdult()) {
+			System.out.println("Seu animal é considerado adulto");
+		} else {
+			System.out.println("Seu animal é considerado filhote");
+		}
+		
+		System.out.println("");
+		
+		System.out.println("Olá, seu animal é da espécie " + gato.getSpecies());
+		System.out.println("O nome do seu animal é " + gato.getName());
+		System.out.println("A idade do seu animal é de " + gato.getAge() + " anos");
+		if (gato.isAdult()) {
 			System.out.println("Seu animal é considerado adulto");
 		} else {
 			System.out.println("Seu animal é considerado filhote");
